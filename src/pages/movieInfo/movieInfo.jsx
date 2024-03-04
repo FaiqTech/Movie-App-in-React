bfcc73a5205b237ec1da795b5783b31f1f78b5b7
FaiqTech <hesenovfaiq@icloud.com>
2024-03-04 13:35:46 +0400

    .

--- src/pages/movieInfo/movieInfo.jsx
+++ src/pages/movieInfo/movieInfo.jsx
@@ -1,31 +1,42 @@
+// MovieInfo.jsx
 import { useLocation } from "react-router-dom";
 import { SMALL_IMG_COVER_BASE_URL } from "../../config";
 import { useEffect, useState } from "react";
 
 const MovieInfo = () => {
   const location = useLocation();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   const { state } = location;
-  const { title, overview, poster_path, release_date, vote_average } = state;
+
   useEffect(() => {
     setData(state);
   }, [state]);
+
+  console.log(state);
+
+  // Arka plan görüntüsü stili
+  const backdropStyle = {
+    backgroundImage: `url(${SMALL_IMG_COVER_BASE_URL + data.backdrop_path})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    minHeight: "500px",
+  };
+
   return (
     <div>
-      <h1>Movie Info</h1>
-      <div>
+      <div className="backdrop" style={backdropStyle}></div>
+      <div className="movie-info">
+        <h1>{data.title}</h1>
+        <p>{data.release_date}</p>
+        <p>{data.overview}</p>
+        <p>{data.vote_average}</p>
         <img
           src={SMALL_IMG_COVER_BASE_URL + data.poster_path}
           alt={data.title}
         />
-        <div>
-          <h2>{data.title}</h2>
-          <p>{data.release_date}</p>
-          <p>{data.overview}</p>
-          <p>{data.vote_average}</p>
-        </div>
       </div>
     </div>
   );
 };
+
 export default MovieInfo;
